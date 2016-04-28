53b66a9f23927375821c4b5443ad913eaacb7e95
John Rodriguez <john@John-Rodriguezs-MacBook-Pro.local>
2016-04-28 17:09:23 -0400

    Added a bunch of stuff like tern, snippets, company-mode

--- init.el
+++ init.el
@@ -7,15 +7,19 @@
  '(custom-safe-themes
    (quote
     ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "405b0ac2ac4667c5dab77b36e3dd87a603ea4717914e30fcf334983f79cfd87e" default)))
+ '(inhibit-startup-screen t)
+ '(initial-buffer-choice t)
  '(js2-global-externs (quote ("describe" "it" "spyOn" "beforeEach" "expect")))
- '(js2-include-node-externs t))
+ '(js2-include-node-externs t)
+ '(show-paren-mode t)
+ '(tool-bar-mode nil))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(linum ((t (:background "brightblack" :foreground "color-232")))))
+ '(linum ((t (:background "Yellow" :foreground "Black")))))
 
 (set-keyboard-coding-system nil)
 (setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
@@ -41,6 +45,10 @@
 (load-theme 'mustard t)
 
 
+;; Make sure path is loaded if using GUI emacs
+(exec-path-from-shell-initialize)
+
+
 ;; Mouse scrolling
 (defun up-slightly () (interactive) (scroll-up 1))
 (defun down-slightly () (interactive) (scroll-down 1))
@@ -53,14 +61,15 @@
 (projectile-global-mode t)
 (global-linum-mode t)
 (global-evil-surround-mode t)
-(global-auto-complete-mode t)
+;; (global-auto-complete-mode t)
+(global-company-mode t)
 (global-flycheck-mode t)
 (global-color-identifiers-mode)
 
 
 ;; Use rainbow-delimiters-mode and rainbow-identifiers-mode for all programming modes
 (add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
-;;(add-hook 'prog-mode-hook 'rainbow-identifiers-mode)
+(add-hook 'prog-mode-hook 'rainbow-identifiers-mode)
 
 
 ;; Remove trailing whitespace before saving a file
@@ -90,7 +99,6 @@
 (add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.scss\\'" . css-mode))
 
-
 ;; Flycheck
 (setq-default flycheck-disabled-checkers
               (append flycheck-disabled-checkers
@@ -102,6 +110,14 @@
 (flycheck-add-mode 'javascript-eslint 'web-mode)
 (flycheck-add-mode 'javascript-eslint 'js2-mode)
 
+
+;; Indent JSON files 2 spaces instead of 4
+(add-hook 'json-mode-hook
+          (lambda ()
+            (make-local-variable 'js-indent-level)
+            (setq js-indent-level 4)))
+
+
 ;; Disable backup files
 (setq make-backup-files nil)
 
@@ -112,6 +128,9 @@
 
 
 ;; Tern.js support
+
+(add-to-list 'company-backends 'company-tern)
+
 (add-hook 'js2-mode-hook (lambda () (tern-mode t)))
 (eval-after-load 'tern
   '(progn
